55a9114b21a257c5a253a56cec4aaf6b7db38d99
MariyaMikhaylova <mikhailovams87@gmail.com>
2024-03-17 16:02:55 +0300

    Скриншот

--- Автоматизация теста к API/test_create_order_and_get_order_by_track.py
+++ Автоматизация теста к API/test_create_order_and_get_order_by_track.py
@@ -1,6 +1,8 @@
 import data
 import sender_stand_request
 
+#Мария Михайлова, 14-я когорта - Финальный проект. Инженер по тестированию плюс
+
 def test_create_order_and_get_order_by_track():
     order = data.create_order_request.copy()
     create_order_response = sender_stand_request.create_new_order(order)
